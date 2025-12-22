c8418e5f9dbed84ca5ae9f18b2098843d11821a3
Dom58 <dndahimana58@gmail.com>
2025-12-22 13:10:47 +0200

    Refactoring the code

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,8 +9,10 @@ export default function Home() {
         <h1 className="text-3xl font-extrabold">Rwanda data management</h1>
         <h1 className="text-xl font-medium">All in 1 search</h1>
         <ul className="mt-4 space-y-1 list-disc list-inside">
-          <li className="text-sm">A centralized system for managing Rwanda's UPI and National ID data.</li>
-          <li className="text-sm">Facilitates easy access and retrieval of citizen information.</li>
+          <li className="text-sm">A centralized system for managing Rwanda's UPI.</li>
+          <li>Allow users to search for UPI data & view it on google maps</li>
+          <li>Allow users to get the real distance to travelfrom his current location <br/> to reach the searchable UPI.</li>
+          <li className="text-sm">Facilitates easy access and retrieval of citizen information for his national ID.</li>
         </ul>
         <div className="mt-6">
           <Link href="/upi" className="text-lg font-bold text-orange-600 underline hover:text-orange-700">
