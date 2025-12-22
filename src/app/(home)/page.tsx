c9526f1eae085cb0cf399fca097dfaaf04c33d10
Dom58 <dndahimana58@gmail.com>
2025-12-22 15:29:30 +0200

    show and hide what we offer

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,42 +1,58 @@
 "use client";
 
 import Link from "next/link";
+import React from "react";
 
 export default function Home() {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
     <div className="flex items-center">
       <main>
         <h1 className="text-3xl font-bold">All in one finder</h1>
         <h1 className="font-medium text-orange-600 text-md">Centralized Rwanda data management</h1>
-        <div className="p-2 mt-2 rounded-lg bg-gray-50/10">
-          <ul className="mt-1 space-y-1 text-sm list-disc list-inside">
-            <li>A centralized system for managing Rwanda's UPI.</li>
-            <li>Allow users to search for UPI data & view it on google maps</li>
-            <li>Allow users to get the real distance from the current location to reach the UPI.</li>
-            <li>Easy access and retrieval of citizen information from his national ID.</li>
-          </ul>
+        <div>
+          <div
+            className="block mb-2 text-sm text-blue-500 underline rounded-lg lg:hidden"
+            onClick={toggleVisibility}
+          >
+            {isVisible ? 'Hide what we offer' : 'Show what we offer'}
+          </div>
+
+          <div className={`lg:block p-2 mt-1 rounded-lg bg-gray-50/10 ${isVisible ? 'block' : 'hidden'} lg:block`}>
+            <ul className="mt-1 space-y-1 text-sm list-disc list-inside">
+              <li>A centralized system for managing Rwanda's UPI.</li>
+              <li>Allow users to search for UPI data & view it on Google Maps.</li>
+              <li>Allow users to get the real distance from the current location to reach the UPI.</li>
+              <li>Easy access and retrieval of citizen information from his national ID.</li>
+            </ul>
+          </div>
         </div>
 
-        <div className="mt-6">
-          <Link href="/upi" className="p-2 px-4 mt-6 font-bold text-orange-600 bg-white border-amber-50 rounded-3xl text-md hover:text-orange-700 hover:bg-gray-100">
+        <div className="flex mt-2 mb-2">
+          <Link href="/upi" className="p-2 px-4 mt-1 text-orange-600 bg-white border-amber-50 rounded-3xl text-md hover:text-orange-700 hover:bg-gray-100">
             Search UPI information
           </Link>
         </div>
 
-        <div className="mt-6 mb-2">
-          <Link href="/citizen-information" className="p-2 px-4 font-bold text-orange-600 bg-white border-amber-50 text-md rounded-3xl hover:text-orange-700 hover:bg-gray-100">
+        <div className="flex mb-2">
+          <Link href="/citizen-information" className="p-2 px-4 text-orange-600 bg-white border-amber-50 text-md rounded-3xl hover:text-orange-700 hover:bg-gray-100">
             Search national ID information
           </Link>
         </div>
 
-        <div className="mt-6 mb-2">
-          <Link href="/#iremboApplications" className="p-2 px-4 font-bold text-orange-600 bg-white border-amber-50 text-md rounded-3xl hover:text-orange-700 hover:bg-gray-100">
+        <div className="flex mb-2">
+          <Link href="/#iremboApplications" className="p-2 px-4 text-orange-600 bg-white border-amber-50 text-md rounded-3xl hover:text-orange-700 hover:bg-gray-100">
             Search your Irembo application status
           </Link>
         </div>
 
-        <div className="mt-6 mb-2">
-          <Link href="/#forexRates" className="p-2 px-4 font-bold text-orange-600 bg-white border-amber-50 text-md rounded-3xl hover:text-orange-700 hover:bg-gray-100">
+        <div className="flex mb-2">
+          <Link href="/#forexRates" className="p-2 px-4 text-orange-600 bg-white border-amber-50 text-md rounded-3xl hover:text-orange-700 hover:bg-gray-100">
             View current forex rates
           </Link>
         </div>
